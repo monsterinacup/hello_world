4dc9647eefdebce7df5f93db06f5516d2d2135a8
monsterinacup <h.n.trewhitt@gmail.com>
2013-06-14 12:35:04 +0100

    Hello World on rails 1st commit
    First ever rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '234af766e141e81747c24fa33d68f8fce2adf0d4b7f43e2f7de488908eaa222667f433b215d396369ea50e36e7dabd42160cf20f104ccf91d4a9133ad30f9521'
